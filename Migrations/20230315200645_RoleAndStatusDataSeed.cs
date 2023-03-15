b326ddc10e9d6c45312aee4391b098714c66d9f9
Tomasz <trnosal@gmail.com>
2023-03-15 21:09:27 +0100

    Add migration from seed data for Role and Status

---
+++ Migrations/20230315200645_RoleAndStatusDataSeed.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseAPI.Migrations
+{
+    public partial class RoleAndStatusDataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Status_Orders_OrderId",
+                table: "Status");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Status_OrderId",
+                table: "Status");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Status");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "OrderPicker" },
+                    { 2, "Warehouseman" },
+                    { 3, "Planner" },
+                    { 4, "Administrator" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Status",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Waiting" },
+                    { 2, "InPreparation" },
+                    { 3, "Prepared" },
+                    { 4, "Sent" },
+                    { 5, "Rejected" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StatusId",
+                table: "Orders",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Status_StatusId",
+                table: "Orders",
+                column: "StatusId",
+                principalTable: "Status",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Status_StatusId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StatusId",
+                table: "Orders");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Status",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Status_OrderId",
+                table: "Status",
+                column: "OrderId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Status_Orders_OrderId",
+                table: "Status",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
