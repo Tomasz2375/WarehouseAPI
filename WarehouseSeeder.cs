5e3df864fdb6865293d2c5e6c3c5e569742bdf44
Tomasz <trnosal@gmail.com>
2023-03-16 20:58:42 +0100

    Add class WarehouseSeeder

---
+++ WarehouseSeeder.cs
@@ -0,0 +1,13 @@
+﻿using WarehouseAPI.Entities;
+
+namespace WarehouseAPI
+{
+    public class WarehouseSeeder
+    {
+        private readonly WarehouseDbContext _dbContext;
+        public WarehouseSeeder(WarehouseDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
