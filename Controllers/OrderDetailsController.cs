c75c5d829b72b3b10de38604a2fa7613f0e68734
Tomasz <trnosal@gmail.com>
2023-03-25 10:50:53 +0100

    Add order details update

--- Controllers/OrderDetailsController.cs
+++ Controllers/OrderDetailsController.cs
@@ -15,10 +15,17 @@ namespace WarehouseAPI.Controllers
         }
         [HttpPost]
         public ActionResult AddOrderDetails
-            ([FromRoute] int orderId, [FromBody]AddOrderDetailsDto dto)
+            ([FromRoute] int orderId, [FromBody] AddOrderDetailsDto dto)
         {
             var id = _orderDetailsService.Create(orderId, dto);
             return Created($"api/order/{orderId}/orderDetails/{id}", null);
+        }
+        [HttpPut("{orderDetailsId}")]
+        public ActionResult UpdateOrderDetails
+            ([FromRoute] int orderId, [FromRoute] int orderDetailsId, [FromBody] int quantity)
+        {
+            var id = _orderDetailsService.Update(orderId, orderDetailsId, quantity);
+            return Ok($"api/order/{orderId}/orderDetails/{id}");
         }
 
     }
--- Services/OrderDetailsService.cs
+++ Services/OrderDetailsService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using WarehouseAPI.Entities;
 using WarehouseAPI.Exceptions;
 using WarehouseAPI.Models;
@@ -10,6 +12,7 @@ namespace WarehouseAPI.Services
     public interface IOrderDetailsService
     {
         int Create(int orderId, AddOrderDetailsDto dto);
+        int Update(int orderId, int orderDetailsId, int quantity);
     }
 
     public class OrderDetailsService : IOrderDetailsService
@@ -34,6 +37,22 @@ namespace WarehouseAPI.Services
             _dbContext.Add(addOrder);
             _dbContext.SaveChanges();
             return addOrder.Id;
+        }
+        public int Update(int orderId, int orderDetailsId, int quantity)
+        {
+            var order = _dbContext.Orders.FirstOrDefault(o => o.Id == orderId);
+            if(order is null)
+            {
+                throw new NotFoundException("Order not found");
+            }
+            var orderDetails = _dbContext.OrderDetails.FirstOrDefault(o => o.Id == orderDetailsId);
+            if (orderDetails is null || orderDetails.OrderId != orderId)
+            {
+                throw new NotFoundException("This order deteils does not exist in currant context");
+            }
+            orderDetails.Quantity = quantity;
+            _dbContext.SaveChanges();
+            return orderDetails.Id;              
         }
     }
 }
