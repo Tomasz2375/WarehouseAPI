4cfa7d0f50591df1a4d7e593929f7f2db8bffdc7
Tomasz <trnosal@gmail.com>
2023-03-27 12:24:00 +0200

    Add migration with Client table

---
+++ Migrations/20230327095926_ClientAddressTPH.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseAPI.Migrations
+{
+    public partial class ClientAddressTPH : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_EmployeeId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Addresses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Addresses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_ClientId",
+                table: "Addresses",
+                column: "ClientId",
+                unique: true,
+                filter: "[ClientId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_EmployeeId",
+                table: "Addresses",
+                column: "EmployeeId",
+                unique: true,
+                filter: "[EmployeeId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Client_ClientId",
+                table: "Addresses",
+                column: "ClientId",
+                principalTable: "Client",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Client_ClientId",
+                table: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_ClientId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_EmployeeId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_EmployeeId",
+                table: "Addresses",
+                column: "EmployeeId",
+                unique: true);
+        }
+    }
+}
